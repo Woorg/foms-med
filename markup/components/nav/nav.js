67f5df03f9ec889a39d0d7565f566a0a52ab32ed
Igor Gorlov <gorlov35@gmail.com>
2021-12-14 03:14:46 +0300

    add pp main page

--- markup/components/nav/nav.js
+++ markup/components/nav/nav.js
@@ -5,7 +5,6 @@ class Nav {
         this.$navTrigger = document.querySelector('.nav__trigger');
         this.$nav = document.querySelector('.nav_primary');
         this.$page = document.querySelector('.page');
-
         this.events();
         this.navOpen = false;
     }
@@ -17,43 +16,50 @@ class Nav {
 
             if ( this.navOpen ) {
 
-                // this.navOpen = true
                 this.closeNav();
 
             } else {
-                // this.navOpen = false
                 this.openNav();
             }
 
         });
 
+        window.addEventListener( 'resize', (e) => {
+            this.resizeNav();
+        });
+
+
+
     }
 
     openNav() {
-        // this.$header.classList.add('header_nav-open');
-        this.$nav.classList.add('nav_open');
-        // this.$navWrap.classList.add('header__nav-appearance');
-        // this.$navWrap.classList.remove('header__nav-disappear');
-
-        // this.$navTrigger.classList.add('nav__trigger_active');
-        this.$page.classList.add('page_nav_open');
 
         this.navOpen = true;
-        console.log('open');
+
+        if ( $(window).width() > '960' ) {
+            this.$nav.classList.remove('nav_open');
+            this.$page.classList.remove('page_nav_open');
+        } else {
+            this.$page.classList.add('page_nav_open');
+            this.$nav.classList.add('nav_open');
+        }
+
     }
 
     closeNav() {
-        // this.$header.classList.remove('header_nav-open');
-        // this.$navWrap.classList.remove('header__nav-appearance');
-        // this.$navWrap.classList.add('header__nav-disappear');
-
-        // this.$navTrigger.classList.remove('nav__trigger_active');
         this.$nav.classList.remove('nav_open');
         this.$page.classList.remove('page_nav_open');
 
         this.navOpen = false;
-        console.log('close');
     }
+
+    resizeNav() {
+        if ( $(window).width() > '960' ) {
+            this.$nav.classList.remove('nav_open');
+            this.$page.classList.remove('page_nav_open');
+        }
+    }
+
 
 
     // methods
